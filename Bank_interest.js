c39f18f4f82e1bbcd0eca39c539cb3d5b397829e
MinaMohsen466 <105835285+MinaMohsen466@users.noreply.github.com>
2023-05-26 14:48:16 -0700

    upgrade_Bank_interest

--- Bank_interest.js
+++ Bank_interest.js
@@ -2,13 +2,14 @@ var
     calc_button = document.getElementById("calc"),
     month_button = document.getElementById("month");
 
-calc_button.onclick = function calc(){
+calc_button.onclick = function calc(Num_of_years = 1){
 
     var Balance = document.getElementById("f_input").value,
         Bank_interest = document.getElementById("l_input").value,
+        Num_of_years = document.getElementById("N_input").value,
         result = document.getElementById("result");
-
-        result.textContent = Balance * Bank_interest / 100 + " In year";
+        
+        result.textContent = Balance * Bank_interest * Num_of_years / 100 + " In "+ Num_of_years +" years";
         result.style.padding = ".6rem";
 }
 
@@ -16,9 +17,10 @@ month_button.onclick = function calc1(){
 
     var Balance = document.getElementById("f_input").value,
         Bank_interest = document.getElementById("l_input").value,
+        Num_of_years = document.getElementById("N_input").value,
         result = document.getElementById("result");
 
         result.style.padding = ".6rem";
-        result.textContent = Balance * Bank_interest / 100 /12 + " In month";
+        result.textContent = Balance * Bank_interest * Num_of_years / 100 / (Num_of_years * 12) + " In month";
 }
 
